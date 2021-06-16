ace71282660fd8737275e9eb305f891ac1f0031d
Sabi Singh (04005424) <SINGHS2@student.op.ac.nz>
2021-06-16 13:50:38 +1200

    deleted extra folder Project

--- project/app/src/main/java/op/mobile/app/dev/singhs2/travelling/model/GitHubCountries.kt
+++
@@ -1,7 +0,0 @@
-package op.mobile.app.dev.singhs2.travelling.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class GitHubCountries(val id: String, val name: String, val capital: String, val flagImg: String) : Parcelable
--- project/app/src/main/java/op/mobile/app/dev/singhs2/travelling/service/ServiceStatus.kt
+++
@@ -1,7 +0,0 @@
-package op.mobile.app.dev.singhs2.travelling.service
-
-enum class ServiceStatus {
-    LOADING,
-    ERROR,
-    COMPLETE
-}
